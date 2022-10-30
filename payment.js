4cb3b8e3de6c691fab0030bdf0586bfc672a9139
CalebTek <walexjackson@gmail.com>
2022-10-30 05:55:58 -0700

    Copy payment.js from original in onramp

---
+++ payment.js
@@ -0,0 +1,8 @@
+let newPaymet = 9000;
+let oldPayment = 5000;
+
+if (newPaymet > oldPayment) {
+  console.log(`You have balance in your account of ${newPaymet - oldPayment}`);
+} else {
+  console.log(`You have no balance in your account`);
+}
